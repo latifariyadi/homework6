6e23c7c897b60c10e774f1e21c9dff3634164884
jemblonganvalley <jemblonganvalley@gmail.com>
2021-07-26 14:47:41 +0800

    up

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,22 +4,56 @@ import "./search.css";
 
 function Search() {
   const [search, setSearch] = useState([]);
+  const [find, setFind] = useState([]);
   useEffect(() => {
     // console.log(data);
-    setSearch(data);
+    search.filter((item) => {
+      setSearch(data);
+    });
   }, []);
+
+  const handleSearch = (inputValue) => {
+    setFind(
+      data
+        .filter((e) => {
+          return (
+            e.artists[0].name.toLowerCase() ==
+            inputValue.target.value.toLowerCase()
+          );
+        })
+        .map((e) => {
+          return e;
+        })
+    );
+  };
+
   return (
     <div className="searchBar">
-      {console.log(search)}
-      <input type="text" className="search-input" />
-      {search
-        .filter((item) => {
-          item.artists == "Queen";
-        })
-        .map((result) => (
-          <h1>result</h1>
-        ))}
-
+      <input
+        type="text"
+        className="search-input"
+        name="search"
+        onChange={handleSearch}
+      />
+      {find.map((e) => (
+        <div
+          style={{
+            width: "300px",
+            padding: "10px",
+            backgroundColor: "white",
+            boxShadow: "4px 5px 8px rgba(0,0,0,0.2)",
+            display: "flex",
+            gap: "10px",
+            marginLeft: "200px",
+          }}
+        >
+          <img
+            src={e.album.images[0].url}
+            style={{ width: 50, height: 50, objectFit: "cover" }}
+          />
+          <p>{e.artists[0].name}</p>
+        </div>
+      ))}
       <button>Cari</button>
     </div>
   );
